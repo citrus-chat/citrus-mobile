3d05be713182c6e2517b86f391410b99c5efe9bb
dottox <imsm2424@gmail.com>
2026-04-24 03:36:59 -0300

    feat: finished pre-hook

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 ----
 
 - 📱 Available for Android 10+
+
+## For developing
+
+- Clone the repository.
+- Use `git config core.hooksPath hooks/` to set up the hooks.
+- Don't develop on the `main` branch, create a new branch for your work.
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,18 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
+    id("dev.detekt") version "2.0.0-alpha.2"
+}
+
+subprojects {
+    // 1. Apply the plugin
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    // 2. Configure using the string name to avoid unresolved reference errors
+    val ktlintExt = extensions.getByName("ktlint")
+    (ktlintExt as org.gradle.api.plugins.ExtensionAware).withGroovyBuilder {
+        setProperty("android", true)
+        setProperty("verbose", true)
+    }
 }
