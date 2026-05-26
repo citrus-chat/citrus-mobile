47aa6658b221ab60a68d4c819f2b6a47fb1988d2
dottox <imsm2424@gmail.com>
2026-05-25 22:08:44 -0300

    added something missing

--- app/src/main/java/com/citruschat/citrusmobile/ui/home/component/ChatListItemComponent.kt
+++ app/src/main/java/com/citruschat/citrusmobile/ui/home/component/ChatListItemComponent.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.citruschat.citrusmobile.domain.model.Chat
 import com.citruschat.citrusmobile.domain.model.ChatListItemSummary
 
 @Composable
