ec873aafbaf6524ebe58bb36fab8f8f0a3c414fb
dottox <imsm2424@gmail.com>
2026-06-22 06:37:35 -0300

    fixing problem with lint

--- app/src/main/java/com/citruschat/citrusmobile/ui/devices/ConnectedDevicesScreen.kt
+++ app/src/main/java/com/citruschat/citrusmobile/ui/devices/ConnectedDevicesScreen.kt
@@ -2,9 +2,7 @@ package com.citruschat.citrusmobile.ui.devices
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,24 +10,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Devices
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.citruschat.citrusmobile.R
 import com.citruschat.citrusmobile.ui.devices.component.ConnectedDeviceRow
 import com.citruschat.citrusmobile.ui.devices.component.ConnectedDevicesHeader
 import com.citruschat.citrusmobile.ui.devices.component.EmptyConnectedDevicesState
@@ -69,4 +56,3 @@ fun ConnectedDevicesScreen(
         }
     }
 }
-
